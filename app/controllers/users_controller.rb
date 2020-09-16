8cd742a265adce34534b60253c39f5d3af75f968
fdfragoso <fefragoso@gmail.com>
2020-09-16 17:22:58 +0200

    Fix rubocop issues

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,35 @@
 class UsersController < ApplicationController
-    def new
-        @user = User.new
-    end
+  def new
+    @user = User.new
+  end
 
-    def create
-        #@user = User.new(username: params[:username], email: params[:email], password: [:password])
-        @user = User.new(user_params)
-        
-        if @user.save
-            redirect_to new_user_path
-        else
-            render :new
-        end
-    end
+  def create
+    # @user = User.new(username: params[:username], email: params[:email], password: [:password])
+    @user = User.new(user_params)
 
-    def edit
-        @user = User.find(params[:id])
+    if @user.save
+      redirect_to new_user_path
+    else
+      render :new
     end
+  end
 
-    def update
-        @user = User.find(params[:id])
-        @user.update(user_params)
-  
-        # flash.notice = "Article '#{@article.title}' Updated!"
-      
-        redirect_to edit_user_path(@user)
-    end
+  def edit
+    @user = User.find(params[:id])
+  end
 
-    private
-     def user_params
-        params.require(:user).permit(:username,:email,:password)
-     end
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+
+    # flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to edit_user_path(@user)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
