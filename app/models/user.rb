b73ca58fab2ac5c89bacf385ea39d57c8e29ca71
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-16 18:10:27 +0300

    Add email validation regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
     validates :username, uniqueness: true, length: { in: 3..20 }
-    validates :email, presence: true
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
     validates :password, presence: true
 end
