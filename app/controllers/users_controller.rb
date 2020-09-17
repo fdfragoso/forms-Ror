a23fe498113897a94775b09d00382c948fb0a977
keddo <adu.kedir.abdurahman@gmail.com>
2020-09-17 14:57:48 +0300

    Redirect update action, leave commented html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,11 +20,11 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-
-    # flash.notice = "Article '#{@article.title}' Updated!"
-
-    redirect_to edit_user_path(@user)
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :new
+    end
   end
 
   private
